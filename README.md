9f01d8895a9d21ea6dcafbc9b0aee206ceeda2aa
Lisa Barton <lisabarton23@yahoo.com>
2021-04-25 14:06:36 -0600

    added questions to quiz

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CodeQuiz
+This webpage is a short quiz with timer to test your knowledge of Code.
--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,74 @@
 var timeElement = document.querySelector("time");
-var questionElement = documnet.querySelector ("question")
-var secondsLeft = 20
+var questionElement = document.querySelector ("question");
+var secondsLeft = 20;
+var startQuiz = document.getElementById ("startButton");
+var submitAnswer = document.getElementById ("submitButton");
 
-function setTime () {
+
+//function needs to go through each questions one at a time
+// with only correct answers advancing to the next questions,
+//also the timer needs to be counting down with more extra 5 
+//seconds taken off if question is answered wrong
+
+function startQuiz () { 
+    
+    // alert ("hello");
     
     var timer = setInterval(function() { 
     secondsLeft--;
-    timeElement.textContent = secondsLeft + " seconds left to answer question";
+     timeElement.textContent = secondsLeft + quest1
 
-    if(secondsLeft === 0) {
+      if(secondsLeft === 0) {
       
-      clearInterval(timerInterval);
+        clearInterval(timerInterval);
       
-      nextQuestion();
-    }
+       nextQuestion();
+     };
 
-  }, 2000);
-};
-
-addEventListener("click")
+   }, 2000);
 
 
+buttonFunction.addEventListener ("click", (startQuiz)); }
+//then start button needs to disapper until quiz is finished//
 
-//questions 5 total, with multiple choice
-//1. Which language would be best to use to specify the layout of a webpage?
-// a. JavaScript
-//b. HTML
-//c. CSS (the answer)
 
-//2. What symbol is used as the universal selector?
-//a. $
-//b. #
-//c. *
+var quizQuestions = [ {
+//questions 5 total, with multiple choice boxes all correct answers are c 
+ question : "1. Which language would be best to use to specify the layout of a webpage?",
+ answers : {
+  a: "JavaScript",
+ b: "HTML",
+ c: "CSS"},
+ correctAnswer: "c"},
 
-//3. Which of the following is NOT a semantic element?
-//a. header
-//b. h1
-//c. div
 
-//4. What JavaScript data type has a value type of true or false?
-//a. String
-//b. BigInt
-//c. Boolean
+{ question:  "What symbol is used as the universal selector?", 
+    answers : {
+        a: "$",
+b: "#",
+c: "*"}, 
+correctAnswer : "c"},
 
-//5. What characters do you use to call/run/execute a function?
-//a. {}
-//b. &&
-//c. ()
+ { question : "Which of the following is NOT a semantic element?",
+ answers : { 
+a:" header",
+b: "h1",
+c: "div", },
+correctAnswer : "c"},
+
+{ question: "What JavaScript data type has a value type of true or false?", 
+answers: {
+a: "String",
+b: "BigInt",
+c: "Boolean",},
+correctAnswer : "c"},
+
+
+{question : " What characters do you use to call/run/execute a function?",
+answers: {
+
+a: "{}",
+b: "&&",
+c: "()", },
+correctAnswer : "c"},
+]
