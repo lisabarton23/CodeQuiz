080f8140f35328a6c14c7a13ff9e98e2a5a5b22f
Lisa Barton <lisabarton23@yahoo.com>
2021-04-27 13:57:31 -0600

    added more psuedo code trying to make my functions work

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,138 @@
 var timeElement = document.querySelector("time");
-var questionElement = document.querySelector ("question");
+var questionElement = document.querySelector ("quiz");
 var secondsLeft = 20;
 var startQuiz = document.getElementById ("startButton");
 var submitAnswer = document.getElementById ("submitButton");
+var results = document.getElementById ("answer");
 
+//A. trivia game 
+//B. TODO: add time
+//C. TODO: add local storage for high score and user to input initals
+
+var index=0;
+
+  var quizQuestions = 
+  [ 
+    {
+      //questions 5 total, with multiple choice boxes all correct answers are c 
+      question : "1. Which language would be best to use to specify the layout of a webpage?",
+      choices : ["JavaScript", "HTML", "CSS"],
+      answer: "CSS"
+    },
+    {
+      
+      question : "2. Which symbol is known as the universal selector?",
+      choices : ["!", "%", "*"],
+      answer: "*"
+    },
+    {
+      
+      question : "What data type would '589' be in javascript?",
+      choices : ["number", "boolean", "string"],
+      answer: "string"
+    },
+
+    {
+      
+      question : "Which of the following is NOT a semantic element?",
+      choices : ["<header>", "<p>", "<div>"],
+      answer: "<div>"
+    },
+    {
+      
+      question : "To call/execute/run run a function what characters must follow the function?",
+      choices : [";", "{}", "()"],
+      answer: "()"
+    },
+  ];
+
+//display what needs to go when we start the game
+//display a startbtn
+//display instructions 
+//display  a title
+  function start(){
+    document.getElementById("displayConent").style.display = "none";
+    document.getElementById("highscoreContent").style.display = "none";
+    document.getElementById("gameOver").style.display = "none";
+
+  }
+  //https://www.w3schools.com/jsref/prop_style_display.asp
+  
+//1. create your array of obj
+//2. create a displaycard() fx ..
+  //display the quest, choices, ans
+  function displaycard(){
+    document.getElementById("startContent").style.display = "none";
+    document.getElementById("highscoreContent").style.display = "none";
+    document.getElementById("gameOver").style.display = "none";
+    document.getElementById("displayConent").style.display = "block";
+
+    //display questions
+    document.getElementById("question").textContent= (quizQuestions[index].question);
+    // console.log(quizQuestions[index].question)
+    //display choices
+    for(var choiceoption=0;choiceoption < quizQuestions[index].choices.length;choiceoption++){
+     var btn = document.createElement("button");
+     btn.textContent=(quizQuestions[index].choices[choiceoption]);
+     btn.setAttribute("id","userOption");
+     btn.setAttribute("data-val",quizQuestions[index].choices[choiceoption]);
+     document.querySelector("#choiceArea").appendChild(btn); 
+     btn.addEventListener ("click", function () {if  (quizQuestions[index].answer === btn) displaycard (quizQuestions [index++].question);},
+     //try adding event.target
+    //  else { timeElement--}
+    //   // 
+       
+    //     document.getElementById("question").textContent= (quizQuestions[1].question)
+
+     )
+
+      
+  }
+      
+  }//TODO: score can be seconds left 
+    
+    //display ans
+// console.log (quizQuestions[index].answer)
+
+  
+//3. TODO: when user onclick choice evaluate if ans ==choice
+  //right: next question displaycard()
+  //wrong: take time off the timer, move to next question displaycard()
+
+
+
+
+  //when the user onclick the startbtn then we call the displaycard() to play the game
+  document.querySelector("#startButton").addEventListener ("click", (displaycard))
 
 //function needs to go through each questions one at a time
 // with only correct answers advancing to the next questions,
 //also the timer needs to be counting down with more extra 5 
 //seconds taken off if question is answered wrong
 
-function startQuiz () { 
+// function quizGo () { 
     
-    // alert ("hello");
+//     // alert ("hello");
     
-    var timer = setInterval(function() { 
-    secondsLeft--;
-     timeElement.textContent = secondsLeft + quest1
+//     var timer = setInterval(function() { 
+//     secondsLeft--;
+//     //  timeElement.textContent = secondsLeft + quizQuestions;??
 
-      if(secondsLeft === 0) {
+//       if(secondsLeft === 0) {
       
-        clearInterval(timerInterval);
-      
-       nextQuestion();
-     };
+//         clearInterval(timerInterval);
+//       //create a results box and highscore initals box, option to restart
+//        ;
+//      };
 
-   }, 2000);
+//    }, 2000);
+// }
 
-
-buttonFunction.addEventListener ("click", (startQuiz)); }
-//then start button needs to disapper until quiz is finished//
-
-
-var quizQuestions = [ {
-//questions 5 total, with multiple choice boxes all correct answers are c 
- question : "1. Which language would be best to use to specify the layout of a webpage?",
- answers : {
-  a: "JavaScript",
- b: "HTML",
- c: "CSS"},
- correctAnswer: "c"},
+// startQuiz.addEventListener ("click", (quizGo)); 
 
 
-{ question:  "What symbol is used as the universal selector?", 
-    answers : {
-        a: "$",
-b: "#",
-c: "*"}, 
-correctAnswer : "c"},
-
- { question : "Which of the following is NOT a semantic element?",
- answers : { 
-a:" header",
-b: "h1",
-c: "div", },
-correctAnswer : "c"},
-
-{ question: "What JavaScript data type has a value type of true or false?", 
-answers: {
-a: "String",
-b: "BigInt",
-c: "Boolean",},
-correctAnswer : "c"},
+//then sart button needs to disapper until quiz is finished//
 
 
-{question : " What characters do you use to call/run/execute a function?",
-answers: {
 
-a: "{}",
-b: "&&",
-c: "()", },
-correctAnswer : "c"},
-]
+start();
